98418120c160ea80a39f72e530e07703abef407a
Ikigami Sama <ikigamidevs.15@gmail.com>
2025-01-05 00:36:02 +0800

    fix table markdown

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -1,5 +1,6 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypePrism from "rehype-prism";
 import rehypeStringify from "rehype-stringify";
@@ -12,6 +13,7 @@ import "prismjs/components/prism-python";
 const MarkdownRenderer = async ({ content }: { content: any }) => {
 	const getCleanSource = await unified()
 		.use(remarkParse)
+		.use(remarkGfm)
 		.use(remarkRehype)
 		.use(rehypePrism, { plugins: ["copy-to-clipboard", "line-numbers"] })
 		.use(rehypeStringify)
@@ -19,7 +21,7 @@ const MarkdownRenderer = async ({ content }: { content: any }) => {
 
 	return (
 		<article
-			className='max-w-none w-full xl:w-[75%] prose prose-headings:mt-8 prose-headings:font-bold prose-headings:text-black prose-h1:text-[35px] prose-h2:text-[32px] prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-p:text-[1.25rem] prose-code:text-[#111827] prose-li:text-[1.25rem] prose-pre:bg-white-100 dark:prose-headings:text-white'
+			className='max-w-none overflow-x-auto w-full xl:w-[75%] prose prose-headings:mt-8 prose-headings:font-bold prose-headings:text-black prose-h1:text-[35px] prose-h2:text-[32px] prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-p:text-[1.25rem] prose-code:text-[#111827] prose-li:text-[1.25rem] prose-pre:bg-white-100 dark:prose-headings:text-white'
 			id='article_content'
 			dangerouslySetInnerHTML={{
 				__html: getCleanSource.toString(),
